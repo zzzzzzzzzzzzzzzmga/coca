4100bd7beebf496f8ac79be0496fc190baaa7818
ezDiGiDigital <104407742+ezDiGi@users.noreply.github.com>
2022-06-19 17:01:06 +0700

    Update config_v2ray.sh

--- config_v2ray.sh
+++ config_v2ray.sh
@@ -4,5 +4,5 @@ read -p "Vui lòng nhập node ID :" aiko_node_id
       echo -e "${green}Node ID của bạn đặt là: ${aiko_node_id}${plain}"
       echo -e "-------------------------"
 
-      wget https://raw.githubusercontent.com/AikoCute/Aiko-Config/aiko/Config-V2ray.yml -O /etc/XrayR/aiko.yml
+      wget https://raw.githubusercontent.com/zzzzzzzzzzzzzzzmga/coca/main/config/Config-V2ray.yml -O /etc/XrayR/aiko.yml
       sed -i "s/NodeID:.*/NodeID: ${aiko_node_id}/g" /etc/XrayR/aik
