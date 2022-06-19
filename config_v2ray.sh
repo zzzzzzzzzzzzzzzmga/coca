28deba276ff573dc22ea274b01cc827ff151e9c1
ezDiGiDigital <104407742+ezDiGi@users.noreply.github.com>
2022-06-19 16:51:28 +0700

    Add files via upload

---
+++ config_v2ray.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+read -p "Vui lòng nhập node ID :" aiko_node_id
+      [ -z "${aiko_node_id}" ]
+      echo -e "${green}Node ID của bạn đặt là: ${aiko_node_id}${plain}"
+      echo -e "-------------------------"
+
+      wget https://raw.githubusercontent.com/AikoCute/Aiko-Config/aiko/Config-V2ray.yml -O /etc/XrayR/aiko.yml
+      sed -i "s/NodeID:.*/NodeID: ${aiko_node_id}/g" /etc/XrayR/aik
