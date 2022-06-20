263e84301894f336e092ed1f52826b3ddcb36a6e
ezDiGiDigital <104407742+ezDiGi@users.noreply.github.com>
2022-06-20 11:00:04 +0700

    Update config_v2ray.sh

--- config_v2ray.sh
+++ config_v2ray.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
-read -p "Vui lòng nhập node ID :" aiko_node_id
-      [ -z "${aiko_node_id}" ]
-      echo -e "${green}Node ID của bạn đặt là: ${aiko_node_id}${plain}"
-      echo -e "-------------------------"
+#install key_path
+    echo -e "[${Green}Key Hợp Lệ${plain}] Link Web : https://www.ezdigi.biz"
+    read -p " ID nút (Node_ID):" node_id
+    [ -z "${node_id}" ] && node_id=0
+    echo "-------------------------------"
+    echo -e "Node_ID: ${node_id}"
+    echo "-------------------------------"
+
+# giới hạn tốc độ
+    read -p " Giới hạn tốc độ (Mbps):" limit_speed
+    [ -z "${limit_speed}" ] && limit_speed=0
+    echo "-------------------------------"
+    echo -e "Giới hạn tốc độ: ${limit_speed}"
+    echo "-------------------------------"
+
+# giới hạn thiết bị
+    read -p " Giới hạn thiết bị (Limit):" limit
+    [ -z "${limit}" ] && limit=0
+    echo "-------------------------------"
+    echo -e "Limit: ${limit}"
+    echo "-------------------------------"
 
       wget https://raw.githubusercontent.com/zzzzzzzzzzzzzzzmga/coca/main/config/Config-V2ray.yml -O /etc/XrayR/aiko.yml
       wget https://raw.githubusercontent.com/ezDiGi/keypem/main/AikoBlock -O /etc/XrayR/AikoBlock
       wget https://raw.githubusercontent.com/zzzzzzzzzzzzzzzmga/coca/main/docker-compose.yml -O /etc/XrayR/docker-compose.yml
-      sed -i "s/NodeID:.*/NodeID: ${aiko_node_id}/g" /etc/XrayR/aiko.yml
+      sed -i "s|NodeID:.*|NodeID: ${node_id}|" ./etc/XrayR/aiko.yml
+      sed -i "s|DeviceLimit:.*|DeviceLimit: ${limit}|" ./etc/XrayR/aiko.yml
+      sed -i "s|SpeedLimit:.*|SpeedLimit: ${limit_speed}|" ./etc/XrayR/aiko.yml
