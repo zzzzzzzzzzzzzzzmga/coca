1b30a8eef42cbab2c48c927c9831ea8f476141d1
ezDiGiDigital <104407742+ezDiGi@users.noreply.github.com>
2022-06-21 19:50:48 +0700

    Create testlai.sh

---
+++ testlai.sh
@@ -0,0 +1,409 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+# Current folder
+cur_dir=$(pwd)
+# Color
+red='\033[0;31m'
+green='\033[0;32m'
+#yellow='\033[0;33m'
+plain='\033[0m'
+operation=(Install Update UpdateConfig logs restart delete)
+# Make sure only root can run our script
+[[ $EUID -ne 0 ]] && echo -e "[${red}Error${plain}] Chưa vào root kìa !, vui lòng xin phép ROOT trước!" && exit 1
+
+#Check system
+check_sys() {
+  local checkType=$1
+  local value=$2
+  local release=''
+  local systemPackage=''
+
+  if [[ -f /etc/redhat-release ]]; then
+    release="centos"
+    systemPackage="yum"
+  elif grep -Eqi "debian|raspbian" /etc/issue; then
+    release="debian"
+    systemPackage="apt"
+  elif grep -Eqi "ubuntu" /etc/issue; then
+    release="ubuntu"
+    systemPackage="apt"
+  elif grep -Eqi "centos|red hat|redhat" /etc/issue; then
+    release="centos"
+    systemPackage="yum"
+  elif grep -Eqi "debian|raspbian" /proc/version; then
+    release="debian"
+    systemPackage="apt"
+  elif grep -Eqi "ubuntu" /proc/version; then
+    release="ubuntu"
+    systemPackage="apt"
+  elif grep -Eqi "centos|red hat|redhat" /proc/version; then
+    release="centos"
+    systemPackage="yum"
+  fi
+
+  if [[ "${checkType}" == "sysRelease" ]]; then
+    if [ "${value}" == "${release}" ]; then
+      return 0
+    else
+      return 1
+    fi
+  elif [[ "${checkType}" == "packageManager" ]]; then
+    if [ "${value}" == "${systemPackage}" ]; then
+      return 0
+    else
+      return 1
+    fi
+  fi
+}
+
+# Get version
+getversion() {
+  if [[ -s /etc/redhat-release ]]; then
+    grep -oE "[0-9.]+" /etc/redhat-release
+  else
+    grep -oE "[0-9.]+" /etc/issue
+  fi
+}
+
+# CentOS version
+centosversion() {
+  if check_sys sysRelease centos; then
+    local code=$1
+    local version="$(getversion)"
+    local main_ver=${version%%.*}
+    if [ "$main_ver" == "$code" ]; then
+      return 0
+    else
+      return 1
+    fi
+  else
+    return 1
+  fi
+}
+
+get_char() {
+  SAVEDSTTY=$(stty -g)
+  stty -echo
+  stty cbreak
+  dd if=/dev/tty bs=1 count=1 2>/dev/null
+  stty -raw
+  stty echo
+  stty $SAVEDSTTY
+}
+error_detect_depends() {
+  local command=$1
+  local depend=$(echo "${command}" | awk '{print $4}')
+  echo -e "[${green}Info${plain}] Bắt đầu cài đặt các gói ${depend}"
+  ${command} >/dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    echo -e "[${red}Error${plain}] Cài đặt gói không thành công ${red}${depend}${plain}"
+    exit 1
+  fi
+}
+
+# Pre-installation settings
+pre_install_docker_compose() {
+#install key_path
+    echo -e "[${Green}Key Hợp Lệ${plain}] Link Web : https://www.ezdigi.biz"
+    read -p " ID nút (Node_ID):" node_id
+    [ -z "${node_id}" ] && node_id=0
+    echo "-------------------------------"
+    echo -e "Node_ID: ${node_id}"
+    echo "-------------------------------"
+
+# giới hạn tốc độ
+    read -p " Giới hạn tốc độ (Mbps):" limit_speed
+    [ -z "${limit_speed}" ] && limit_speed=0
+    echo "-------------------------------"
+    echo -e "Giới hạn tốc độ: ${limit_speed}"
+    echo "-------------------------------"
+
+# giới hạn thiết bị
+    read -p " Giới hạn thiết bị (Limit):" limit
+    [ -z "${limit}" ] && limit=0
+    echo "-------------------------------"
+    echo -e "Limit: ${limit}"
+    echo "-------------------------------"
+}
+ 
+
+
+# Config docker
+config_docker() {
+  cd ${cur_dir} || exit
+  echo "Bắt đầu cài đặt các gói"
+  install_dependencies
+  echo "Tải tệp cấu hình DOCKER"
+  cat >docker-compose.yml <<EOF
+version: '3'
+services: 
+  xrayr: 
+    image: aikocute/xrayr:latest
+    volumes:
+      - ./config.yml:/etc/XrayR/config.yml # thư mục cấu hình bản đồ
+      - ./dns.json:/etc/XrayR/dns.json 
+    restart: always
+    network_mode: host
+EOF
+  cat >dns.json <<EOF
+{
+    "servers": [
+        "1.1.1.1",
+        "8.8.8.8",
+        "localhost"
+    ],
+    "tag": "dns_inbound"
+}
+EOF
+
+
+EOF
+  cat >AikoBlock <<EOF
+catport.vn
+cloudfast.vn
+zingfast.net
+zingfast.vn
+speedtest.net
+fast.com
+speedtest.vn
+speedsmart.net
+speedcheck.org
+speedof.me
+testmy.net
+bandwidthplace.com
+speed.io
+measurementlab.net
+i-speed.vn
+speedtest.vnpt.vn
+speedtest.vtn.com.vn
+nperf.com
+speedtest.telstra.com
+merter.net
+ping-test.net
+devicetests.com
+speedtest.com.sg
+speed.cloudflare.com
+speedtest.vinahost.vn
+thinkbroadband.com
+speedtestcustom.com
+speedtest.vegacdn.com
+ooklaserver.net
+speedtest.cesnet.cz
+speakeasy.net
+speedtest.midco.net
+speedtest.xfinity.com
+speedtest.googlefiber.net
+speedtestcustom.com
+EOF
+
+
+EOF
+  cat >aiko.yml <<EOF
+Log:
+  Level: none # Log level: none, error, warning, info, debug 
+  AccessPath: # /etc/XrayR/access.Log
+  ErrorPath: # /etc/XrayR/error.log
+DnsConfigPath: # /etc/XrayR/dns.json # Path to dns config
+RouteConfigPath: # /etc/XrayR/route.json # Path to route config
+InboundConfigPath: # /etc/XrayR/custom_inbound.json # Path to custom inbound config
+OutboundConfigPath: # /etc/XrayR/custom_outbound.json # Path to custom outbound config
+  Handshake: 4 # Handshake time limit, Second
+  ConnIdle: 86400 # Connection idle time limit, Second
+  UplinkOnly: 2 # Time limit when the connection downstream is closed, Second
+  DownlinkOnly: 4 # Time limit when the connection is closed after the uplink is closed, Second
+  BufferSize: 64 # The internal cache size of each connection, kB 
+Nodes:
+  -
+    PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, Proxypanel
+    ApiConfig:
+      ApiHost: "https://ezdigi.biz"
+      ApiKey: "kenhdidong_mmmzo"
+      NodeID: 1
+      NodeType: V2ray # Node type: V2ray, Shadowsocks, Trojan, Shadowsocks-Plugin
+      Timeout: 30 # Timeout for the api request
+      EnableVless: false # Enable Vless for V2ray Type
+      EnableXTLS: false # Enable XTLS for V2ray and Trojan
+      SpeedLimit: $limit_speed # Mbps, Local settings will replace remote settings, 0 means disable
+      DeviceLimit: $limit # Local settings will replace remote settings, 0 means disable
+      RuleListPath: /root/AikoBlock # /etc/XrayR/AikoBlock Path to local rulelist file
+    ControllerConfig:
+      ListenIP: 0.0.0.0 # IP address you want to listen
+      SendIP: 0.0.0.0 # IP address you want to send pacakage
+      UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.
+      EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
+      DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
+      EnableProxyProtocol: false # Only works for WebSocket and TCP
+      EnableFallback: false # Only support for Trojan and Vless
+      FallBackConfigs:  # Support multiple fallbacks
+        -
+          SNI: # TLS SNI(Server Name Indication), Empty for any
+          Alpn: # Alpn, Empty for any
+          Path: # HTTP PATH, Empty for any
+          Dest: 80 # Required, Destination of fallback
+          ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
+      CertConfig:
+        CertMode: none # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
+        CertDomain: "node1.test.com" # Domain to cert
+        CertFile: /etc/XrayR/cert/node1.test.com.cert # Provided if the CertMode is file
+        KeyFile: /etc/XrayR/cert/node1.test.com.key
+        Provider: alidns # DNS cert provider, Get the full support list 
+        Email: test@me.com
+        DNSEnv: # DNS ENV option used by DNS provider
+          ALICLOUD_ACCESS_KEY: aaa
+          ALICLOUD_SECRET_KEY: bbb
+EOF
+  sed -i "s|NodeID:.*|NodeID: ${node_id}|" ./aiko.yml
+  sed -i "s|DeviceLimit:.*|DeviceLimit: ${limit}|" ./aiko.yml
+  sed -i "s|SpeedLimit:.*|SpeedLimit: ${limit_speed}|" ./aiko.yml
+}
+
+# Install docker and docker compose
+install_docker() {
+  echo -e "bắt đầu cài đặt DOCKER "
+ sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+systemctl start docker
+systemctl enable docker
+  echo -e "bắt đầu cài đặt Docker Compose "
+curl -fsSL https://get.docker.com | bash -s docker
+curl -L "https://github.com/docker/compose/releases/download/1.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+  echo "khởi động Docker "
+  service docker start
+  echo "khởi động Docker-Compose "
+  docker-compose up -d
+  echo
+  echo -e "Đã hoàn tất cài đặt phụ trợ ！"
+  echo -e "0 0 */3 * *  cd /root/${cur_dir} && /usr/local/bin/docker-compose pull && /usr/local/bin/docker-compose up -d" >>/etc/crontab
+  echo -e "Cài đặt cập nhật thời gian kết thúc đã hoàn tất! hệ thống sẽ update sau [${green}24H${plain}] Từ lúc bạn cài đặt"
+}
+
+install_check() {
+  if check_sys packageManager yum || check_sys packageManager apt; then
+    if centosversion 5; then
+      return 1
+    fi
+    return 0
+  else
+    return 1
+  fi
+}
+
+install_dependencies() {
+  if check_sys packageManager yum; then
+    echo -e "[${green}Info${plain}] Kiểm tra kho EPEL ..."
+    if [ ! -f /etc/yum.repos.d/epel.repo ]; then
+      yum install -y epel-release >/dev/null 2>&1
+    fi
+    [ ! -f /etc/yum.repos.d/epel.repo ] && echo -e "[${red}Error${plain}] Không cài đặt được kho EPEL, vui lòng kiểm tra." && exit 1
+    [ ! "$(command -v yum-config-manager)" ] && yum install -y yum-utils >/dev/null 2>&1
+    [ x"$(yum-config-manager epel | grep -w enabled | awk '{print $3}')" != x"True" ] && yum-config-manager --enable epel >/dev/null 2>&1
+    echo -e "[${green}Info${plain}] Kiểm tra xem kho lưu trữ EPEL đã hoàn tất chưa ..."
+
+    yum_depends=(
+      curl
+    )
+    for depend in ${yum_depends[@]}; do
+      error_detect_depends "yum -y install ${depend}"
+    done
+  elif check_sys packageManager apt; then
+    apt_depends=(
+      curl
+    )
+    apt-get -y update
+    for depend in ${apt_depends[@]}; do
+      error_detect_depends "apt-get -y install ${depend}"
+    done
+  fi
+  echo -e "[${green}Info${plain}] Đặt múi giờ thành Hồ Chí Minh GTM+7"
+  ln -sf /usr/share/zoneinfo/Asia/Ho_Chi_Minh  /etc/localtime
+  date -s "$(curl -sI g.cn | grep Date | cut -d' ' -f3-6)Z"
+
+}
+
+#update_image
+Update_xrayr() {
+  cd ${cur_dir}
+  echo "Tải hình ảnh DOCKER"
+  docker-compose pull
+  echo "Bắt đầu chạy dịch vụ DOCKER"
+  docker-compose up -d
+}
+
+#show last 100 line log
+
+logs_xrayr() {
+  echo "100 dòng nhật ký chạy sẽ được hiển thị"
+  docker-compose logs --tail 100
+}
+
+# Update config
+UpdateConfig_xrayr() {
+  cd ${cur_dir}
+  echo "đóng dịch vụ hiện tại"
+  docker-compose down
+  pre_install_docker_compose
+  config_docker
+  echo "Bắt đầu chạy dịch vụ DOKCER"
+  docker-compose up -d
+}
+
+restart_xrayr() {
+  cd ${cur_dir}
+  docker-compose down
+  docker-compose up -d
+  echo "Khởi động lại thành công!"
+}
+delete_xrayr() {
+  cd ${cur_dir}
+  docker-compose down
+  cd ~
+  rm -Rf ${cur_dir}
+  echo "đã xóa thành công!"
+}
+# Install xrayr
+Install_xrayr() {
+  pre_install_docker_compose
+  config_docker
+  install_docker
+}
+
+# Initialization step
+clear
+while true; do
+  echo "-----XrayR Aiko-----"
+  echo "Địa chỉ dự án và tài liệu trợ giúp:  https://github.com/AikoCute/XrayR"
+  echo "AikoCute Hột Me"
+  echo "Vui lòng nhập một số để Thực Hiện Câu Lệnh:"
+  for ((i = 1; i <= ${#operation[@]}; i++)); do
+    hint="${operation[$i - 1]}"
+    echo -e "${green}${i}${plain}) ${hint}"
+  done
+  read -p "Vui lòng chọn một số và nhấn Enter (Enter theo mặc định ${operation[0]}):" selected
+  [ -z "${selected}" ] && selected="1"
+  case "${selected}" in
+  1 | 2 | 3 | 4 | 5 | 6 | 7)
+    echo
+    echo "Bắt Đầu : ${operation[${selected} - 1]}"
+    echo
+    ${operation[${selected} - 1]}_xrayr
+    break
+    ;;
+  *)
+    echo -e "[${red}Error${plain}] Vui lòng nhập số chính xác [1-6]"
+    ;;
+  esac
+done
